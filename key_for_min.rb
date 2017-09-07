553f95f455b0b7abea52eb8dae16c415acdddf35
Justin Tooley <jstooley823@gmail.com>
2017-09-07 06:43:54 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,7 +4,8 @@
 def key_for_min_value(name_hash)
   min_value =0
   min_key = nil
-  name_hash.each_with_index do |key, value, i|
+  i = 0
+  name_hash.each do |key, value|
      if i == 0
        min_key = key
        min_value = value
@@ -12,6 +13,7 @@ def key_for_min_value(name_hash)
        min_key = key
        min_value = value
      end
+     i += 1
      min_key
   end
 
